77bf074c6f52fded4d661efc53095070fa117e9c
Not Officer <marlon.john007@gmail.com>
2021-04-14 21:01:28 +0200

    minor refactor

--- src/Discord.Net.Commands/SlashCommands/Types/CommandModule/ISlashCommandModule.cs
+++ src/Discord.Net.Commands/SlashCommands/Types/CommandModule/ISlashCommandModule.cs
@@ -1,13 +1,5 @@
-using Discord.Commands.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Discord.SlashCommands
 {
-
     public interface ISlashCommandModule
     {
         void SetContext(IDiscordInteraction interaction);
