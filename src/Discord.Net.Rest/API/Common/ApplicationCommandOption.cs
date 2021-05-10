b71a5720f1e47244326ad0350ab1905d33dbb2cb
Not Officer <marlon.john007@gmail.com>
2021-05-10 12:53:09 +0200

    fixed registering commands

--- src/Discord.Net.Rest/API/Common/ApplicationCommandOption.cs
+++ src/Discord.Net.Rest/API/Common/ApplicationCommandOption.cs
@@ -1,9 +1,6 @@
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Discord.API
 {
@@ -34,49 +31,38 @@ namespace Discord.API
 
         public ApplicationCommandOption(IApplicationCommandOption cmd)
         {
-            this.Choices = cmd.Choices.Select(x => new ApplicationCommandOptionChoice()
+            Choices = cmd.Choices?.Select(x => new ApplicationCommandOptionChoice
             {
                 Name = x.Name,
                 Value = x.Value
             }).ToArray();
 
-            this.Options = cmd.Options.Select(x => new ApplicationCommandOption(x)).ToArray();
+            Options = cmd.Options?.Select(x => new ApplicationCommandOption(x)).ToArray();
 
-            this.Required = cmd.Required.HasValue
-                ? cmd.Required.Value
-                : Optional<bool>.Unspecified;
-            this.Default = cmd.Default.HasValue
-                ? cmd.Default.Value
-                : Optional<bool>.Unspecified;
+            Required = cmd.Required ?? Optional<bool>.Unspecified;
+            Default = cmd.Default ?? Optional<bool>.Unspecified;
 
-            this.Name = cmd.Name;
-            this.Type = cmd.Type;
-            this.Description = cmd.Description;
+            Name = cmd.Name.ToLowerInvariant();
+            Type = cmd.Type;
+            Description = cmd.Description;
         }
-        public ApplicationCommandOption(Discord.ApplicationCommandOptionProperties option)
+
+        public ApplicationCommandOption(ApplicationCommandOptionProperties option)
         {
-            this.Choices = option.Choices != null
-                ? option.Choices.Select(x => new ApplicationCommandOptionChoice()
-                {
-                    Name = x.Name,
-                    Value = x.Value
-                }).ToArray()
-                : Optional<ApplicationCommandOptionChoice[]>.Unspecified;
+            Choices = option.Choices?.Select(x => new ApplicationCommandOptionChoice
+            {
+                Name = x.Name,
+                Value = x.Value
+            }).ToArray() ?? Optional<ApplicationCommandOptionChoice[]>.Unspecified;
 
-            this.Options = option.Options != null
-                ? option.Options.Select(x => new ApplicationCommandOption(x)).ToArray()
-                : Optional<ApplicationCommandOption[]>.Unspecified;
+            Options = option.Options?.Select(x => new ApplicationCommandOption(x)).ToArray() ?? Optional<ApplicationCommandOption[]>.Unspecified;
 
-            this.Required = option.Required.HasValue
-                ? option.Required.Value
-                : Optional<bool>.Unspecified;
-            this.Default = option.Default.HasValue
-                ? option.Default.Value
-                : Optional<bool>.Unspecified;
+            Required = option.Required ?? Optional<bool>.Unspecified;
+            Default = option.Default ?? Optional<bool>.Unspecified;
 
-            this.Name = option.Name;
-            this.Type = option.Type;
-            this.Description = option.Description;
+            Name = option.Name.ToLowerInvariant();
+            Type = option.Type;
+            Description = option.Description;
         }
     }
 }
