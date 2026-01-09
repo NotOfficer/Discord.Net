023a93cb8cead29899ff6e224dc398a0cd522a4d
Marlon <marlon.john@hotmail.de>
2026-01-09 05:24:02 +0100

    doc fix

--- src/Discord.Net.Commands/SlashCommands/Attributes/CommandGroup.cs
+++ src/Discord.Net.Commands/SlashCommands/Attributes/CommandGroup.cs
@@ -1,15 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Discord.SlashCommands
 {
     /// <summary>
     ///     Defines the current as being a group of slash commands.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Class)]
     public class CommandGroup : Attribute
     {
         /// <summary>
@@ -25,7 +21,8 @@ namespace Discord.SlashCommands
         /// <summary>
         ///     Tells the <see cref="SlashCommandService"/> that this class/function is a slash command.
         /// </summary>
-        /// <param name="commandName">The name of this slash command.</param>
+        /// <param name="groupName">The name of this command group.</param>
+        /// <param name="description">The description of this command group.</param>
         public CommandGroup(string groupName, string description = "No description.")
         {
             this.groupName = groupName.ToLower();
